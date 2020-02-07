8e2b8a51aee279ccc5e5160ce41cb861845e4fa0
PaulSouille <paul.souille85@gmail.com>
2020-02-07 08:31:14 +0100

    add pagination

--- front/src/MainPage/MainPage.js
+++ front/src/MainPage/MainPage.js
@@ -1,6 +1,26 @@
 import * as React from "react";
 
+function getQueryVariable(variable)
+{
+        var query = window.location.search.substring(1);
+        console.log(query)//"app=article&act=news_content&aid=160990"
+        var vars = query.split("&");
+        console.log(vars) //[ 'app=article', 'act=news_content', 'aid=160990' ]
+        for (var i=0;i<vars.length;i++) {
+                    var pair = vars[i].split("=");
+                    console.log(pair)//[ 'app', 'article' ][ 'act', 'news_content' ][ 'aid', '160990' ] 
+        if(pair[0] == variable){return pair[1];}
+         }
+         return(false);
+}
+const startAt = parseInt(getQueryVariable('startAt'))+10
+const startAtPrevious = parseInt(getQueryVariable('startAt'))-10
+const hrefValueNext = '?startAt=' +startAt
+const hrefValuePrevious= '?startAt=' +startAtPrevious
 export default ({ products }) => <div className="container-fluid">
+    <a href={hrefValuePrevious}>Précédent </a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href={hrefValueNext}>Suivant</a>
   <br></br>
   {products.length > 0 &&
     <div align="left" className="row">
@@ -22,4 +42,5 @@ export default ({ products }) => <div className="container-fluid">
       </div>
     </div>
   }
+
   </div>
