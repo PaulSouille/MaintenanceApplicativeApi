7e320573da96b28d7e3699eca2b629b994efae1d
PaulSouille <paul.souille85@gmail.com>
2020-02-07 08:30:09 +0100

    add pagination

--- front/src/MainPage/index.js
+++ front/src/MainPage/index.js
@@ -2,4 +2,23 @@ import products from '../data';
 import MainPage from "./MainPage";
 import React from 'react'
 
-export default () => <MainPage products={products} />
+function getQueryVariable(variable)
+{
+        var query = window.location.search.substring(1);
+        console.log(query)//"app=article&act=news_content&aid=160990"
+        var vars = query.split("&");
+        console.log(vars) //[ 'app=article', 'act=news_content', 'aid=160990' ]
+        for (var i=0;i<vars.length;i++) {
+                    var pair = vars[i].split("=");
+                    console.log(pair)//[ 'app', 'article' ][ 'act', 'news_content' ][ 'aid', '160990' ] 
+        if(pair[0] == variable){return pair[1];}
+         }
+         return(false);
+}
+
+const startAt = getQueryVariable('startAt');
+const endAt = parseInt(startAt)+10
+console.log(endAt)
+const products_sliced = products.slice(startAt,endAt)
+
+export default () => <MainPage products={products_sliced} />
